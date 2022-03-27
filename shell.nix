b7690c48030ef847de4604f69f395519d3d6bd1d
Vincent Khougaz <vincent@khougaz.com>
2022-03-26 17:38:51 -0700

    sigrok-cli, pulseview, Arduino working on darwin

--- shell.nix
+++ shell.nix
@@ -1,38 +1,12 @@
 {}:
 let
-  unstable-pkgs = import (builtins.fetchTarball {
-    # qtgamepad was broken on nixos-21.11, a fix came out a month later
-    name = "nixos-unstable-2022-3-20";
-    url = "https://github.com/nixos/nixpkgs/archive/3b3a69db09be401aca15436ae879ae0b1aac5dfd.tar.gz";
-    sha256 = "0xjrshbcak5g8cakk0m7b4qy179agqbf2v79196xmkm7x5z8h7z5";
-  }) {};
-
-  setqt5 = self: super: {
-    qt5 = unstable-pkgs.qt5;
-    qtwebkit = super.qtwebkit;
-  };
-
-  setdarwin = self: super: {
-    pulseview-darwin = super.pulseview.overrideAttrs (old: {
-      meta = {
-        
-      };
-    });
-  };
-
-  stable-pkgs = import (builtins.fetchTarball {
+  pkgs = import (builtins.fetchTarball {
     name = "nixos-21.11";
     url = "https://github.com/nixos/nixpkgs/archive/d2caa9377539e3b5ff1272ac3aa2d15f3081069f.tar.gz";
     sha256 = "0syx1mqpdm37zwpxfkmglcnb1swqvsgf6sff0q5ksnsfvqv38vh0";
-  }) {
-    overlays = [
-      # setqt5
-    ];
-  };
+  }) {};
 
-  pkgs = stable-pkgs; 
-
-  stdenv = pkgs.stdenv;
+  inherit (pkgs) lib stdenv;
 
   arduino-darwin = stdenv.mkDerivation rec {
     pname = "arduino";
@@ -50,57 +24,27 @@ let
     '';
   };
 
-  pulseview-darwin-manual = stdenv.mkDerivation rec {
-    pname = "pulseview";
-    version = "0.4.1";
-
-    src = pkgs.fetchurl {
-      url = "http://sigrok.org/download/source/pulseview/${pname}-${version}.tar.gz";
-      sha256 = "0bvgmkgz37n2bi9niskpl05hf7rsj1lj972fbrgnlz25s4ywxrwy";
-    };
-
-    buildInputs = with pkgs; [
-      pkgconfig
-      cmake
-      glib
-      qt5.full
-      boost
-      libsigrok
-      libsigrokdecode
-      libserialport
-      libzip
-      # udev
-      libusb1
-      libftdi1
-      glibmm
-      pcre librevisa python3
-     
-    ];
-
-  };
-
   pulseview-darwin = pkgs.pulseview.overrideAttrs (old: {
+    # the same as previous except without "udev"
     buildInputs = with pkgs; [
       glib boost libsigrok libsigrokdecode libserialport libzip libusb1 libftdi1 glibmm
       pcre librevisa python3
-      qt5.qtbase qt5.qtsvg
+      qt514.qtbase qt514.qtsvg
     ];
-    meta = old.meta // {
-      platforms = pkgs.lib.platforms.darwin;
-    };
+    meta = old.meta // (with lib; {
+      platforms = platforms.darwin;
+    });
   });
 in
 pkgs.mkShell {
     buildInputs = [
       arduino-darwin
       pulseview-darwin
+      pkgs.sigrok-cli
     ];
     shellHook = ''
-set -e
-export ARDUINO_BIN=${arduino-darwin}/Applications/Arduino.app/Contents/MacOS/
-export SIGROK_BIN=${pulseview-darwin}
-# Needed to fix hanky borken window not resizing in wayland...
-export _JAVA_AWT_WM_NONREPARENTING=1
-export PATH="$PATH:$ARDUINO_BIN"
+      set -e
+      export ARDUINO_BIN=${arduino-darwin}/Applications/Arduino.app/Contents/MacOS/
+      export PATH="$PATH:$ARDUINO_BIN"
     '';
 }
